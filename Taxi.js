2032d0b4d1bfec842785e10fd9054697f5b1eb53
Rimantas <belauzas@yahoo.com>
2022-06-22 12:01:50 +0300

    Taxi solution

--- Taxi.js
+++ Taxi.js
@@ -1,5 +1,32 @@
 class Taxi {
+    constructor(name) {
+        this.name = name;
+        this.totalDistance = 0;
+        this.price = 1;
+        this.profit = 0;
+    }
 
+    formatCurrency(num) {
+        return num.toFixed(2) + ' Eur';
+    }
+
+    intro() {
+        return `Sveiki, as ${this.name} ir kilometro kaina su manimi yra ${this.formatCurrency(this.price)}.`;
+    }
+
+    updatePrice(newPrice) {
+        this.price = newPrice;
+        return `${this.name} nustate nauja kilometro kaina - ${this.formatCurrency(newPrice)}.`;
+    }
+
+    ride(distance) {
+        this.totalDistance += distance;
+        this.profit += distance * this.price;
+    }
+
+    stats() {
+        return `${this.name} nuvaziavo ${this.totalDistance} km ir uzdirbo ${this.formatCurrency(this.profit)}.`;
+    }
 }
 
 module.exports = Taxi;
--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const Taxi = require('./Taxi');
 
+console.clear();
+
 const jonas = new Taxi('Jonas');
 const maryte = new Taxi('Maryte');
 const stasys = new Taxi('Stasys');
 const ona = new Taxi('Ona');
 
-jonas.intro();
+console.log(jonas.intro());
 // Sveiki, as Jonas ir kilometro kaina su manimi yra 1.00 Eur.
-maryte.intro();
+console.log(maryte.intro());
 // Sveiki, as Maryte ir kilometro kaina su manimi yra 1.00 Eur.
-stasys.intro();
+console.log(stasys.intro());
 // Sveiki, as Stasys ir kilometro kaina su manimi yra 1.00 Eur.
-ona.intro();
+console.log(ona.intro());
 // Sveiki, as Ona ir kilometro kaina su manimi yra 1.00 Eur.
 
-jonas.updatePrice(0.9);
+console.log('-------------------');
+
+console.log(jonas.updatePrice(0.9));
 // Jonas nustate nauja kilometro kaina - 0.90 Eur.
-ona.updatePrice(2);
+console.log(ona.updatePrice(2));
 // Ona nustate nauja kilometro kaina - 2.00 Eur.
-maryte.updatePrice(1.2);
+console.log(maryte.updatePrice(1.2));
 // Maryte nustate nauja kilometro kaina - 1.20 Eur.
 
 jonas.ride(5);
@@ -26,14 +30,16 @@ maryte.ride(7);
 stasys.ride(2);
 ona.ride(2);
 
-jonas.stats();
+console.log(jonas.stats());
 // Jonas nuvaziavo 5 km ir uzdirbo 4.50 Eur.
-maryte.stats();
+console.log(maryte.stats());
 // Maryte nuvaziavo 7 km ir uzdirbo 8.40 Eur.
-stasys.stats();
+console.log(stasys.stats());
 // Stasys nuvaziavo 2 km ir uzdirbo 2.00 Eur.
-ona.stats();
+console.log(ona.stats());
 // Ona nuvaziavo 2 km ir uzdirbo 4.00 Eur.
+
+console.log('-------------------');
 
 maryte.ride(13);
 stasys.ride(7);
@@ -48,20 +54,22 @@ maryte.ride(1);
 stasys.ride(9);
 ona.ride(10);
 
-jonas.stats();
+console.log(jonas.stats());
 // Jonas nuvaziavo 18 km ir uzdirbo 16.20 Eur.
-maryte.stats();
+console.log(maryte.stats());
 // Maryte nuvaziavo 24 km ir uzdirbo 28.80 Eur.
-stasys.stats();
+console.log(stasys.stats());
 // Stasys nuvaziavo 30 km ir uzdirbo 30.00 Eur.
-ona.stats();
+console.log(ona.stats());
 // Ona nuvaziavo 33 km ir uzdirbo 66.00 Eur.
 
-jonas.updatePrice(1.5);
+console.log('-------------------');
+
+console.log(jonas.updatePrice(1.5));
 // Jonas nustate nauja kilometro kaina - 1.50 Eur.
-ona.updatePrice(2.3);
+console.log(ona.updatePrice(2.3));
 // Ona nustate nauja kilometro kaina - 2.30 Eur.
-stasys.updatePrice(1.5);
+console.log(stasys.updatePrice(1.5));
 // Stasys nustate nauja kilometro kaina - 1.50 Eur.
 
 jonas.ride(5);
@@ -73,11 +81,11 @@ maryte.ride(1);
 stasys.ride(9);
 ona.ride(10);
 
-jonas.stats();
+console.log(jonas.stats());
 // Jonas nuvaziavo 27 km ir uzdirbo 29.70 Eur.
-maryte.stats();
+console.log(maryte.stats());
 // Maryte nuvaziavo 28 km ir uzdirbo 33.60 Eur.
-stasys.stats();
+console.log(stasys.stats());
 // Stasys nuvaziavo 51 km ir uzdirbo 61.50 Eur.
-ona.stats();
-// Ona nuvaziavo 33 km ir uzdirbo 102.80 Eur.
+console.log(ona.stats());
+// Ona nuvaziavo 49 km ir uzdirbo 102.80 Eur.
